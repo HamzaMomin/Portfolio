63a8be92d1a3325a815795ec1a6cc3f6ea239bec
Hamza Momin <65024078+HamzaMomin@users.noreply.github.com>
2024-03-04 20:34:12 +0500

    changess

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './navbar.css'
+const NavBar = () => {
+
+    return (
+
+        <div>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li className="nav-item">Home</li>
+                    <li className="nav-item">About</li>
+                    <li className="nav-item">Contact</li>
+                </ul>
+            </nav>
+
+        </div>
+    )
+
+}
+
+export default NavBar
