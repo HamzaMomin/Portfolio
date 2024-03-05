5c5d6e244b26b7bf8d4bb2c155c3c8fcaa1e524b
Hamza Momin <65024078+HamzaMomin@users.noreply.github.com>
2024-03-05 23:56:53 +0500

    Feat: Updates

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,17 +1,25 @@
+// Intro.js
+
 import React from "react";
-import Hamza2 from '../../assets/Hamza2.png'
-const Intro = () =>{
+import Hamza2 from '../../assets/Hamza2.png';
+import './intro.css';
 
+const Intro = () => {
     return (
-
-        <section id="intro">
+        <div className="container">
             <div className="intro-content">
-            <span className="hello">Hello,</span>
+                <h2 className="h2">Hello,</h2>
+                <h3 className="introText">I'm <span className="introName">Hamza Momin</span>
+                    <br />a Web Developer
+                    <p className="intropara">I am a skilled web developer with experience in creating visually and appealing web applications.</p>
+                </h3>
+                <button className="btn"> Hire Me! </button>
             </div>
-            <img src={Hamza2} alt="profile" className="bg"/> 
-        </section>
-
-)
+            <div className="imageCard">
+                <img src={Hamza2} alt="profile" className="bg" />
+            </div>
+        </div>
+    )
 }
 
 export default Intro;
