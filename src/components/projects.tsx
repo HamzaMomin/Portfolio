ea530030d47d64a293bb5b9fe4c006d2530cb83e
Hamza Momin <hamzamomin5@gmail.com>
2025-03-20 23:48:52 +0500

    feat: projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,8 +6,8 @@ import "slick-carousel/slick/slick-theme.css";
 
 interface ExperienceCard {
     ProjectTitle: string;
-    ProjectType: string;
     ProjectURL: string;
+   
     des: string[];
     background: string;
 }
@@ -15,28 +15,34 @@ interface ExperienceCard {
 const experiences: ExperienceCard[] = [
     {
         ProjectTitle: "phostrack.com",
-        ProjectType: "Project Type: Freelance",
         ProjectURL: "https://phostrack.com/",
+       
         des: [
-            "Worked on both front-end and back-end using HTML, CSS, Bootstrap, and PHP",
-            "Integrated ACF fields and created dynamic pages",
-            "Fixed website issues, optimized performance, and conducted QA testing",
-            "Developed a custom WordPress plugin to automatically assign alt attributes to images for SEO & accessibility",
-            "Built and customized pages using Elementor and Avada"
+            "Phostrack is a mobile app built using React Native, powered by Hugging Face AI for image generation.",
+            "The landing page is developed with React.js, Tailwind CSS, and TypeScript for a sleek and responsive design.",
+            "Cloudflare is used for hosting, while EmailJS is integrated for seamless custom email services.",
+            
         ],
         background: "/src/assets/phostrack.png"
     },
     {
-        ProjectTitle: "Internship | Junior WordPress Developer",
-        ProjectType: "Xgrid.co",
-        ProjectURL: " ",
+        ProjectTitle: "Poistivus",
+        ProjectURL: "https://github.com/HamzaMomin/Poistivus",
+      
         des: [
-            "Used Elementor and Avada to design and develop visually appealing web pages",
-            "Assisted senior developers in creating custom solutions using HTML, CSS, Bootstrap, and JavaScript",
-            "Conducted QA testing and prepared detailed reports on website functionality",
-            "Identified bugs and performance issues"
+           "A simple Figma-to-Bootstrap conversion.",
+           "Bootstrap 5 was used to build this website, along with custom CSS for styling.",
+           "This project was created to practice and learn Bootstrap."
         ],
-        background: "/path/to/image2.jpg"
+        background: "/src/assets/positivus.png"
+    },{
+        ProjectTitle: "Real Estate Insignia",
+        ProjectURL: "https://github.com/HamzaMomin/Real_Estate_Insignia_Admin_Panel_Web",
+      
+        des: [
+          
+        ],
+        background: "/src/assets/"
     }
 ];
 
@@ -77,13 +83,13 @@ const WorkExperience = () => {
                         {experiences.map((exp, index) => (
                             <div key={index} className="px-4">
                                 <div className="bg-white rounded-lg p-6 mb-12 border-2 border-black shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] md:mx-24 lg:mx-24 sm:mx-auto relative">
-                                    <div className="w-full h-64 bg-cover bg-center rounded-t-lg border-b-2 border-black" style={{ backgroundImage: `url(${exp.background})` }}></div>
+                                    <div className="w-full h-128 bg-cover rounded-t-lg border-b-2 border-black" style={{ backgroundImage: `url(${exp.background})` }}></div>
 
                                     <div className="flex items-center justify-between p-4">
                                         <div>
                                             <h3 className="text-xl text-black  mt-2 font-bold flex item-start">{exp.ProjectTitle}</h3>
-                                            <p className=' text-gray-900  mt-2  flex item-start'><i>{exp.ProjectType}</i></p>
-                                            <a href={exp.ProjectURL} target="_blank" rel="noopener noreferrer" className=" text-orange-600 hover:text-orange-800 font-semibold mt-2 flex items-start cursor-pointer">
+
+                                            <a href={exp.ProjectURL} target="_blank" rel="noopener noreferrer" className=" text-white  hover:text-black bg-orange-600 border-2 border-black px-4 rounded-2xl  shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]  font-semibold mt-2 flex items-start cursor-pointer">
                                                 View Project
                                             </a>
                                         </div>
@@ -97,6 +103,7 @@ const WorkExperience = () => {
 
                                     {expandedIndex === index && (
                                         <ul className="list-disc text-left list-inside mt-4 space-y-2 text-black text-[18px] bg-white p-4 rounded-lg">
+                                            
                                             {exp.des.map((point, i) => (
                                                 <li key={i}>{point}</li>
                                             ))}
